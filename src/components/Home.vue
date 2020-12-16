78a2db9fd9760427a646fdb2f82478c0c6533f69
Dekpo Yologaza <dekpo@me.com>
2020-12-16 11:54:58 +0100

    Added my own Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Hello VUE 3</h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color:red;
+}
+</style>
